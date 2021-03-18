021925e333a06cd614833092bf7d9ea6cfb58b7b
Daniel <schwartzray8@gmail.com>
2021-03-18 16:17:58 -0500

    modules

--- strassen.cpp
+++ strassen.cpp
@@ -2,10 +2,10 @@
 
 int main(int argc, char const *argv[])
 {  
-	// auto t1 = std::chrono::high_resolution_clock::now();
-	int flag =  atoi(argv[1]);
-	int dim = atoi(argv[2]);
-  int input_file = atoi(argv[3]);
-  std::cout << dim << std::endl;
+    // auto t1 = std::chrono::high_resolution_clock::now();
+    int flag =  atoi(argv[1]);
+    int dim = atoi(argv[2]);
+    int input_file = atoi(argv[3]);
+    std::cout << dim << std::endl;
 
 }
