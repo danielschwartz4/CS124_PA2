62d7f55cc5f8e0984601b675fd2a74d8073132e1
Daniel <schwartzray8@gmail.com>
2021-03-18 21:52:56 -0500

    conventional start

--- strassen.cpp
+++ strassen.cpp
@@ -7,16 +7,6 @@ int main(int argc, char const *argv[])
     int flag =  atoi(argv[1]);
     int dim = atoi(argv[2]);
     int input_file = atoi(argv[3]);
-    int mat1[4][4] = { { 1, 1, 1, 1 },
-                       { 2, 2, 2, 2 },
-                       { 3, 3, 3, 3 },
-                       { 4, 4, 4, 4 } };
- 
-    int mat2[4][4] = { { 1, 1, 1, 1 },
-                       { 2, 2, 2, 2 },
-                       { 3, 3, 3, 3 },
-                       { 4, 4, 4, 4 } };
-    int new_matrix = conventional(mat1, mat2, 4, 4, 4, 4)
     std::cout << dim << std::endl;
 
 }
